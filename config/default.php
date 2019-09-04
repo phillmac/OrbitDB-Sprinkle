84177542e85fd900eb72c0ac9fa15d9621f0afa7
phillmac <phillmac@users.noreply.github.com>
2019-09-04 20:14:14 +1000

    Add config, sprinkle bootstrap, package.json

---
+++ config/default.php
@@ -0,0 +1,26 @@
+<?php
+
+
+    return [
+        'debug' => [
+            'twig' => TRUE
+        ],
+        'site' => [
+            'AdminLTE' => [
+                'skin' => 'green'
+            ],
+            'registration' => [
+                'enabled' => false,
+                'require_email_verification' => false
+             ],
+            'author'    =>      'Phillip Mackintosh',
+            'title'     =>      'OrbitDB',
+            // URLs
+            'uri' => [
+                'author' => 'https://peelvalley.com.au'
+            ]
+        ],
+        'php' => [
+            'timezone' => 'Australia/Sydney'
+        ]
+    ];
---
+++ src/Orbit_Db_Dashboard.php
@@ -0,0 +1,14 @@
+<?php
+namespace UserFrosting\Sprinkle\OrbitDB;
+
+use UserFrosting\System\Sprinkle\Sprinkle;
+
+/**
+ * Bootstrapper class for the 'orbit-db-dashboard' sprinkle.
+ *
+ * @author Phillip Mackintosh (https://peelvalley.com.au)
+ */
+class Orbit_Db_Dashboard extends Sprinkle
+{
+
+}
